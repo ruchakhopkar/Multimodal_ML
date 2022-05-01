c1855226b3efb6ed314aab8925eba973ff5d459c
Ubuntu <ubuntu@ip-172-31-37-240.us-east-2.compute.internal>
2022-05-01 21:26:33 +0000

    base alfred code

--- alfred/models/nn/vnn.py
+++ alfred/models/nn/vnn.py
@@ -272,6 +272,8 @@ class ConvFrameMaskDecoderProgressMonitor(nn.Module):
         progresses = []
         for t in range(max_t):
             action_t, mask_t, state_t, attn_score_t, subgoal_t, progress_t = self.step(enc, frames[:, t], e_t, state_t)
+            print('--------------------------')
+            print('mask_t',mask_t)
             masks.append(mask_t)
             actions.append(action_t)
             attn_scores.append(attn_score_t)
